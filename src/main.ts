f4a26bb0b0bec25a61a04be77f2c4d679036bcbe
ricardo.machado <ricardo.machado@paygo-consult.com.br>
2023-07-19 14:17:21 -0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
   });
   await app.listen(8080);
 }
