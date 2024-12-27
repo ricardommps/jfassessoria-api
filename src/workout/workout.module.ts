402e969c535c4e4b4346b3f5bb10a1a87838a212
Ricardo Matta <ricardommps@gmail.com>
2024-12-27 13:30:03 -0300

    Melhorias

--- src/workout/workout.module.ts
+++ src/workout/workout.module.ts
@@ -2,6 +2,7 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MediaEntity } from 'src/media/entities/media.entity';
 import { ProgramModule } from 'src/program/program.module';
+import { WorkoutLoadModule } from 'src/workout-load/workout-load.module';
 import { WorkoutEntity } from './entities/workout.entity';
 import { WorkoutController } from './workout.controller';
 import { WorkoutService } from './workout.service';
@@ -10,6 +11,7 @@ import { WorkoutService } from './workout.service';
   imports: [
     TypeOrmModule.forFeature([WorkoutEntity, MediaEntity]),
     forwardRef(() => ProgramModule),
+    forwardRef(() => WorkoutLoadModule),
   ],
   providers: [WorkoutService],
   controllers: [WorkoutController],
