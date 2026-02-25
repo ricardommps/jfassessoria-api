1a5a589e41c08ce20daa8b15cd1cb5a379fdabd6
Ricardo Matta <ricardommps@gmail.com>
2026-02-25 07:02:37 -0300

    Atualiza migrations

--- src/migration/1766498328497-alter_table_workouts.ts
+++ src/migration/1766498328497-alter_table_workouts.ts
@@ -3,15 +3,15 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AlterTableWorkouts1766498328497 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-                ALTER TABLE "public"."workouts" 
-                ADD COLUMN "distance" NUMERIC(10,2);
-              `);
+      ALTER TABLE "public"."workouts" 
+      ADD COLUMN "distance" NUMERIC(10,2);
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            ALTER TABLE "public"."workouts" 
-            DROP COLUMN "distance",
-          `);
+      ALTER TABLE "public"."workouts" 
+      DROP COLUMN "distance",
+    `);
   }
 }
--- src/migration/1769363879810-alter_table_workouts.ts
+++ src/migration/1769363879810-alter_table_workouts.ts
@@ -1,17 +1,17 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AlterTableWorkouts1766753320744 implements MigrationInterface {
+export class AlterTableWorkouts1769363879810 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      ALTER TABLE "public"."workouts" 
-      ADD COLUMN "link" character varying;
+            ALTER TABLE "public"."workouts" 
+            ADD COLUMN "muscles_worked" boolean DEFAULT false;
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      ALTER TABLE "public"."workouts" 
-      DROP COLUMN "link",
+        ALTER TABLE "public"."workouts" 
+        DROP COLUMN "muscles_worked",
     `);
   }
 }
